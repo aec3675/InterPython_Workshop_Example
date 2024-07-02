7a7acb4b3b882c236d25b7981f6d70172a97209e
Adrian Crawford <pnr5sh@virginia.edu>
2024-07-02 11:35:37 -0400

    added parametrized min_mag tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -53,15 +53,32 @@ def test_mean_mag(test_df, test_colname, expected):
     assert mean_mag(test_df, test_colname) == expected
 
 
-def test_min_mag_negatives():
-   # Test that min_mag function works for negatives
-   from lcanalyzer.models import min_mag
-
-   test_input_df = pd.DataFrame(data=[[-7, -7, -3], [-4, -3, -1], [-1, -5, -3]], columns=list("abc"))
-   test_input_colname = "b"
-   test_output = -7
-
-   assert min_mag(test_input_df, test_input_colname) == test_output
+@pytest.mark.parametrize(
+        "test_df, test_colname, expected",
+    [
+        (pd.DataFrame(data=[[-7, -7, -3],
+                            [-4, -3, -1],
+                            [-1, -5, -3]],
+                      columns=list("abc")),
+         "a",
+         -7.),
+        (pd.DataFrame(data=[[0, 0, 0], 
+                            [0, 0, 0], 
+                            [0, 0, 0]], 
+                      columns=list("abc")),
+        "b",
+        0),
+        (pd.DataFrame(data=[[1, 5, 3], 
+                            [7, 8, 9], 
+                            [3, 4, 1]], 
+                      columns=list("abc")),
+        "a",
+        pytest.approx(1,0.01))
+    ])
+def test_min_mag(test_df, test_colname, expected):
+    # Test that min_mag function works for positives, negatives, and zeroes
+    from lcanalyzer.models import min_mag
+    assert min_mag(test_df, test_colname) == expected
 
 
 def test_max_mag_strings():
